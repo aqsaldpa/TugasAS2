58f7bcb8575c86c954d649446f5fa9d71ab0b0d1
aqsaldpa <aqsaldpa28@gmail.com>
2021-04-08 10:33:33 +0700

    tugas

---
+++ app/src/main/java/com/example/formactivity/MainActivity2.java
@@ -0,0 +1,51 @@
+package com.example.formactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button toNextActivity,toFragment1,toFragment2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        toNextActivity = findViewById(R.id.toNextActivity);
+        toFragment1 = findViewById(R.id.toFragment1);
+        toFragment2 = findViewById(R.id.toFragment2);
+
+        toNextActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this,MainActivitySecond.class);
+                startActivity(intent);
+            }
+        });
+        toFragment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FragmentFirst());   }
+        });
+        toFragment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FragmentSecond());  }
+        });
+    }
+    private void loadFragment (Fragment fragment){
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.container,fragment);
+        fragmentTransaction.commit();
+    }
+}
